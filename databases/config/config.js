b0233b79b0a98a2530a172a13c6922ee8b108bb2
Alexandre Prado Romeu <pradoromeu@hotmail.com>
2021-08-08 15:09:30 -0300

    Incluído menu na barra lateral

---
+++ databases/config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    "development": {
+        "username": "root",
+        "password": "projeto@123",
+        "database": "projeto",
+        "host": "127.0.0.1",
+        "dialect": "mysql",
+        "operatorAliases": false
+    }
+}
